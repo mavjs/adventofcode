70b4fa7612bc186df5dd494a7583ab0589828fbe
Maverick Kaung <mavjs01@gmail.com>
2021-12-03 16:01:54 +0100

    Completed Day 1!

--- 2021/Day1/src/main.rs
+++ 2021/Day1/src/main.rs
@@ -15,6 +15,30 @@ fn part1(vec: &Vec<i32>) {
     println!("Part 1 sum: {}", sum)
 }
 
+fn part2(vec: &Vec<i32>) {
+    let mut prev_window = vec[0] + vec[1] + vec[2];
+    let mut sum = 0;
+    let mut cur_window;
+
+    for index in 1..vec.len() {
+        if vec.len() - index < 2 {
+            cur_window = vec[index];
+        } else if vec.len() - index < 3 {
+            cur_window = vec[index] + vec[index+1];
+        } else {
+            cur_window = vec[index] + vec[index+1] + vec[index+2];
+        }
+
+
+        if cur_window > prev_window {
+            sum += 1
+        }
+
+        prev_window = cur_window;
+    }
+    println!("Part 2 sum: {}", sum)
+}
+
 fn main() {
     let contents = File::open("data/input.txt").unwrap();
     let reader = BufReader::new(contents);
@@ -26,4 +50,6 @@ fn main() {
     }
     
     part1(&vec);
+
+    part2(&vec);
 }
